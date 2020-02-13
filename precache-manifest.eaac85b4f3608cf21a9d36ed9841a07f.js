7f43e424042feeefb5269c051b10e43d3ea529b3
gonzarascon <gonzarascon@gmail.com>
2020-02-13 01:37:08 -0300

    Updates

---
+++ precache-manifest.eaac85b4f3608cf21a9d36ed9841a07f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64302c68b9b457fa4a8df5bb8f18805c",
+    "url": "/botonera_new/index.html"
+  },
+  {
+    "revision": "7167a040c480863add3e",
+    "url": "/botonera_new/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a21f0d7dc4c1e4566c39",
+    "url": "/botonera_new/static/js/2.39b9ec56.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/botonera_new/static/js/2.39b9ec56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7167a040c480863add3e",
+    "url": "/botonera_new/static/js/main.68a8d194.chunk.js"
+  },
+  {
+    "revision": "6a4f71d40b0c7ec5e127",
+    "url": "/botonera_new/static/js/runtime-main.09a57d8d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/botonera_new/static/media/logo.5d5d9eef.svg"
+  }
+]);
