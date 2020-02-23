8f4de787acc9b32eea6cda53ba9bc5c7a4c63530
gonzarascon <gonzarascon@gmail.com>
2020-02-22 23:27:16 -0300

    Share API

--- src/components/SoundButton/index.js
+++ src/components/SoundButton/index.js
@@ -3,28 +3,43 @@ import _ from "lodash";
 import SVG from "react-inlinesvg";
 import PropTypes from "prop-types";
 import ReactHowler from "react-howler";
-import { Box, Label } from "./styles";
+import { Box, Label, Play, Share } from "./styles";
 
 function SoundButton({ sound, label, color, handlePlay, playerId, playing }) {
   const [loading, setLoading] = useState(true);
   const playerRef = useRef();
 
-  function renderLabel() {
-    if (playing) {
-      return (
-        <SVG
-          src="./assets/icons/pause.svg"
-          title="Pause"
-          className="pause-icon"
-        />
-      );
+  async function shareFile() {
+    const file = await fetch(`/assets/audio/${sound}.mp3`).then(response =>
+      response.blob()
+    );
+    if (navigator.canShare && navigator.canShare({ files: [file] })) {
+      navigator
+        .share({
+          title: sound.toUppercase,
+          text: "Ey encontré esto en la botonera",
+          file,
+        })
+        .then(() => console.log("Compartido exitosamente"))
+        .catch(e => console.error("No se pudo compartir", e));
     }
+  }
 
-    return label;
+  function renderIfShareAvailable() {
+    if (navigator.share) {
+      return (
+        <Share color={color} onClick={() => shareFile()}>
+          <SVG src="./assets/icons/share.svg" title="Share" />
+        </Share>
+      );
+    } else {
+      console.log("Web Share Api no se soporta en tu dispositivo");
+      return null;
+    }
   }
 
   return (
-    <Box color={color} onClick={() => handlePlay(playing ? null : playerId)}>
+    <Box color={color}>
       <ReactHowler
         src={[`./assets/audio/${sound}.mp3`, `./assets/audio/${sound}.ogg`]}
         playing={playing}
@@ -36,12 +51,24 @@ function SoundButton({ sound, label, color, handlePlay, playerId, playing }) {
         ref={playerRef}
         html5={true}
       />
-
       {loading ? (
         <SVG src="./assets/icons/loader.svg" title="Loading" />
       ) : (
-        <Label>{renderLabel()}</Label>
+        <Label>{label}</Label>
       )}
+
+      <Play
+        color={color}
+        onClick={() => handlePlay(playing ? null : playerId)}
+        full={navigator.share ? false : true}
+      >
+        <SVG
+          src={`./assets/icons/${playing ? "pause" : "play"}.svg`}
+          title="Play / Pause"
+        />
+      </Play>
+
+      {renderIfShareAvailable()}
     </Box>
   );
 }
--- src/components/SoundButton/styles.js
+++ src/components/SoundButton/styles.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { breakpoints, colors, Utils } from "../../constants";
 
 const { readColor, neumorphism, calculateRem } = Utils;
@@ -9,22 +9,34 @@ export const Box = styled.div`
   color: ${props =>
     props.color.isDark() ? "white" : readColor(colors.darkText)};
   ${props => neumorphism(props.color)};
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  display: grid;
+
+  grid-template-columns: 70% 1fr 1fr;
+  grid-template-rows: 1fr;
+
+  place-content: center;
   transition: transform ease-in-out 0.3s;
+
+  & > div {
+    display: none;
+  }
 
   @media ${breakpoints.smallDevice} {
     height: 100px;
   }
 
   @media ${breakpoints.mediumDevice} {
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 80% 1fr;
+    padding: 15px;
     height: 150px;
   }
 
   svg {
     width: 50%;
     height: 50%;
+    grid-column: 1/-1;
+    grid-row: 1/-1;
 
     circle {
       stroke: ${props =>
@@ -40,13 +52,6 @@ export const Box = styled.div`
         props.color.isDark() ? "white" : readColor(colors.darkText)};
     }
   }
-
-  @media (hover: hover) {
-    &:hover {
-      transform: scale(1.05);
-      cursor: pointer;
-    }
-  }
 `;
 
 export const Label = styled.span`
@@ -59,4 +64,94 @@ export const Label = styled.span`
   -moz-user-select: none; /* Firefox */
   -ms-user-select: none; /* IE10+/Edge */
   user-select: none;
+
+  align-self: center;
+
+  @media ${breakpoints.mediumDevice} {
+    grid-column: 1/ -1;
+    grid-row: 1 / 2;
+  }
+`;
+
+export const Play = styled.span`
+  svg {
+    width: 30px;
+    height: 30px;
+    margin: 0 auto;
+
+    path {
+      fill: ${props =>
+        props.color.isDark() ? "white" : readColor(colors.darkText)};
+    }
+  }
+      align-self: center;
+    justify-self: center;
+
+    ${props =>
+      props.full &&
+      css`
+        grid-column: 2/-1;
+      `}
+
+  @media ${breakpoints.mediumDevice} {
+    grid-column: 1 / 2;
+    grid-row: 2 / 3;
+justify-self: start;
+    ${props =>
+      props.full &&
+      css`
+        grid-column: 1/-1;
+        justify-self: center;
+      `}
+
+    
+
+    svg {
+      width: 30px;
+      height: 30px;
+      margin: 0 auto;
+    }
+  }
+
+  @media (hover: hover) {
+    &:hover {
+      transform: scale(1.1);
+      cursor: pointer;
+    }
+  }
+`;
+
+export const Share = styled.span`
+  svg {
+    width: 30px;
+    height: 30px;
+    margin: 0 auto;
+
+    path {
+      fill: ${props =>
+        props.color.isDark() ? "white" : readColor(colors.darkText)};
+    }
+  }
+
+  align-self: center;
+  justify-self: center;
+
+  @media ${breakpoints.mediumDevice} {
+    grid-column: 2 / 3;
+    grid-row: 2 / 3;
+    justify-self: end;
+
+    svg {
+      width: 30px;
+      height: 30px;
+      margin: 0 auto;
+    }
+  }
+
+  @media (hover: hover) {
+    &:hover {
+      transform: scale(1.1);
+      cursor: pointer;
+    }
+  }
 `;
